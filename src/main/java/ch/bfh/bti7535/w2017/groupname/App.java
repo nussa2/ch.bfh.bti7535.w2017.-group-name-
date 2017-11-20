86d1b9aa7da5e80420995f27027995c759591a8d
Alex <nussa2@bfh.ch>
2017-11-20 19:00:00 +0100

    Initial Commit

---
+++ src/main/java/ch/bfh/bti7535/w2017/groupname/App.java
@@ -0,0 +1,13 @@
+package ch.bfh.bti7535.w2017.groupname;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
