b88dcc57e19f0ad288306e0cc6b820f898219606
SoullessStone <micheldc@windowslive.com>
2017-11-20 19:34:31 +0100

    added dependency to wekan

--- src/main/java/ch/bfh/bti7535/w2017/groupname/App.java
+++ src/main/java/ch/bfh/bti7535/w2017/groupname/App.java
@@ -1,13 +1,13 @@
 package ch.bfh.bti7535.w2017.groupname;
 
+import weka.classifiers.bayes.NaiveBayes;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        NaiveBayes naiveBayes = new NaiveBayes();
     }
 }
