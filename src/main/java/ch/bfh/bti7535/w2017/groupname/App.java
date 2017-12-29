36db07e5492d29b9bdb45b637b040d546754a2d3
SoullessStone <micheldc@windowslive.com>
2017-12-29 15:12:20 +0100

    % hinzugefügt

--- src/main/java/ch/bfh/bti7535/w2017/groupname/App.java
+++ src/main/java/ch/bfh/bti7535/w2017/groupname/App.java
@@ -11,27 +11,32 @@ public class App {
     public static void main(String[] args) {
         try {
             switch (args[0]) {
-                case "PP_SentimentLexiconPercent":
-                    ChainConfigSentimentPercentLexicon sentimentLexiconPercentChain = new ChainConfigSentimentPercentLexicon();
-                    new DefaultChainProcessor().process(sentimentLexiconPercentChain);
-                    break;
                 case "PP_SentimentLexiconCount":
+                    // 62.3%
                     ChainConfigSentimentLexicon sentimentLexiconChain = new ChainConfigSentimentLexicon();
                     new DefaultChainProcessor().process(sentimentLexiconChain);
                     break;
+                case "PP_SentimentLexiconPercent":
+                    // 63.5%
+                    ChainConfigSentimentPercentLexicon sentimentLexiconPercentChain = new ChainConfigSentimentPercentLexicon();
+                    new DefaultChainProcessor().process(sentimentLexiconPercentChain);
+                    break;
                 case "PP_AttributeSelection":
+                    // 78.3%
                     ChainConfigVectorAttSelect vectorASChain = new ChainConfigVectorAttSelect();
                     new DefaultChainProcessor().process(vectorASChain);
                     break;
                 case "Validate":
-                    // Hier muss man noch ch.bfh.bti7535.w2017.groupname.process.ChainConfigCVNB.addStep anpassen
+                    /* Hier muss man noch ch.bfh.bti7535.w2017.groupname.process.ChainConfigCVNB.addStep
+                        anpassen, damit es das richtige input-file nimmt
+                     */
                     ChainConfigCVNB filterChain = new ChainConfigCVNB();
                     CVEvaluationChainProcessor cvEvaluationChainProcessor = new CVEvaluationChainProcessor();
                     cvEvaluationChainProcessor.process(filterChain);
                     System.out.println("Error Rate: " + cvEvaluationChainProcessor.getValidationResultErrorRate());
                     break;
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
